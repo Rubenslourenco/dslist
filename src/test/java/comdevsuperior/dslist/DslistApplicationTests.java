91e74e2b1e06e16b1104346708bcaf9a251e7519
Rubens Alves <alvesrubens926@gmail.com>
2023-05-11 22:20:46 -0300

    project created

---
+++ src/test/java/comdevsuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package comdevsuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
